5f9b0c1ec8c6f23c68f34319f33e540c1f84242f
Monika Tadross <monikk.tadross@gmail.com>
2019-02-03 19:45:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ function add(a,b) {
   return a + b
 }
 
+
 function subtract(a,b) {
   return a - b
 }
 
+
 function multiply(a,b) {
   return a * b
 }
+
+
 function divide(a,b) {
   return a / b
 }
@@ -18,18 +22,19 @@ function inc(n) {
   return n +1 ;
 }
 
+
 function dec(n) {
   return n - 1;
 }
+
 function makeInt(n, base) {
   var parsed = parseInt(n, 10);
   return parsed;
-
-  
 }
   
-function parseFloat() {
-  
+function parseFloat(n) {
+  var parsed = parseInt(n);
+  return parsed;
 } 
   
 
